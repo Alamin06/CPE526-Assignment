1a93b32b0cddb505469c354f470a39bdb29d6764
Alamin06 <aminusuleiman06@gmail.com>
2021-06-01 14:33:59 +0100

    Getting prime number or composite number as inputed by the user and also applying the first method

--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrimeNumbers {
@@ -6,19 +8,58 @@ public class PrimeNumbers {
         int prime, composite;
         int factors;
 
+        ArrayList numList = new ArrayList();
         System.out.println("Please give a number");
         Scanner sc = new Scanner(System.in);
         number = sc.nextInt();
-        factors = number / 2;
+        int count=1;
+        int itNum=0;
 
-        for (int i = 0; i >= 1; i++) {
-            if (i % 2 == 0) {
-                System.out.print(i + " is composite number and factors are"+ factors);
-            } else {
-                System.out.println(i + " is a prime number");
+        for(int i=2; i<number; i++){
+            if(number%i==0){
+                count++;
             }
+            itNum++;
+        }
 
+        if(count==1){
+            System.out.println( number + " is a prime number and factors are => ");
+            System.out.println("With first method, number of iterations are: " + itNum);
 
         }
+        if(count>1){
+            System.out.print(number + " is composite number and factors are => ");
+
+            for(int i = 2; i < number; i++){
+                if(number%i==0){
+                    System.out.print(i+",");
+                }
+            }
+            System.out.println("\n" +"Our first method number of iteration is: " + itNum);
+        }
+
+
+
+
+//
+//        if (number % 2== 0 && number!=2) {
+//            System.out.print(number + " is composite number and factors are => ");
+//
+//            for(int i = 2; i < number; i++){
+//                if(number%i==0){
+//                    System.out.print(i+",");
+//                    count++;
+//                }
+//            }
+//            System.out.println("\n" +"Our first method number of iteration is: " + count);
+//        }
+//        else {
+//            System.out.println( number + " is a prime number");
+//        }
+//
+//        for (int i = 2; i <= number; i++) {
+//
+//        }
     }
 }
+
