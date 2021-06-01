d3477d0a8ea75c5ac4c8739a65c69cdc1fd84e8b
Alamin06 <aminusuleiman06@gmail.com>
2021-06-01 10:47:16 +0100

    Prompting user to insert a number and then check whether it is a prime or composite number

--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -1,5 +1,24 @@
-public class PrimeNumbers {
-    public static void main{
+import java.util.Scanner;
 
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int number;
+        int prime, composite;
+        int factors;
+
+        System.out.println("Please give a number");
+        Scanner sc = new Scanner(System.in);
+        number = sc.nextInt();
+        factors = number / 2;
+
+        for (int i = 0; i >= 1; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " is composite number and factors are"+ factors);
+            } else {
+                System.out.println(i + " is a prime number");
+            }
+
+
+        }
     }
 }
