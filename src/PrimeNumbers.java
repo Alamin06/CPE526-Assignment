4f785817cba180d5ab2be0c71a44f571b97c5c6d
Alamin06 <aminusuleiman06@gmail.com>
2021-06-01 18:48:26 +0100

    Getting the second method of iteration

--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -1,18 +1,20 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class PrimeNumbers {
     public static void main(String[] args) {
         int number;
         int prime, composite;
-        int factors;
-        
+
         System.out.println("Please give a number");
         Scanner sc = new Scanner(System.in);
         number = sc.nextInt();
         int count=1;
         int itNum=0;
+        int index=2;
+
+        double b=Math.sqrt(number);
+        int x=number;
+
 
         for(int i=2; i<number; i++){
             if(number%i==0){
@@ -21,9 +23,23 @@ public class PrimeNumbers {
             itNum++;
         }
 
+
+            while (x>1 && index<=b ){
+                while (x/index>0){
+
+                    x = x/index;
+                    b = Math.sqrt(x);
+                }
+                index++;
+            }
+
+
         if(count==1){
-            System.out.println( number + " is a prime number and factors are => ");
+            System.out.println( number+ " is a prime number and factors are => ");
             System.out.println("With first method, number of iterations are: " + itNum);
+            System.out.println("With second method, number of iterations are: " + index);
+
+
 
         }
         if(count>1){
@@ -35,11 +51,17 @@ public class PrimeNumbers {
                 }
             }
             System.out.println("\n" +"Our first method number of iteration is: " + itNum);
+            System.out.println("With second method, number of iterations are: " + index);
+
+
+            }
+
+
         }
 
 
 
 
-    }
 }
+
 
